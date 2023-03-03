311a0dc4ac8217cfc2ce0bddc41d3d8bef81239f
bunnany <45374304+bunnany@users.noreply.github.com>
2023-03-03 14:17:12 +1300

    Initial Commit
    Going to commit to the asylum

---
+++ class_scores.py
@@ -0,0 +1,15 @@
+##
+#
+# Author: NYB
+
+# Lists of class scores
+class_A = [44, 33, 28, 47, 12, 28, 32, 31, 11, 39, 40, 26, 36]
+class_B = [19, 26, 38, 31, 30, 42, 44, 14, 27, 43, 46, 49, 24, 26, 36]
+
+# Ask user for name
+name = input("Enter name: ")
+print("Hello, ", name)
+
+# Loop through lists
+for grade in class_A:
+    print(grade)
